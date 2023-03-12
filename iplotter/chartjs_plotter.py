299eab73adb46ae5fbb5dcd4ee942c85410871a1
sentisso <38100632+sentisso@users.noreply.github.com>
2023-03-12 19:32:40 +0000

    downgrade to 3.9.1

--- iplotter/chartjs_plotter.py
+++ iplotter/chartjs_plotter.py
@@ -10,7 +10,7 @@ class ChartJSPlotter(IPlotter):
 
     head = """
     <!-- Load Charts.js -->
-    <script src='https://cdn.jsdelivr.net/npm/chart.js/dist/chart.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js'></script>
     """
 
     template = """
